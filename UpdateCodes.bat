9c56b140d2ad1c15afa703865211b6996b22d98a
Divaddd <43974972+Divaddd@users.noreply.github.com>
2020-02-18 15:58:55 -0500

    Add files via upload

--- UpdateCodes.bat
+++ UpdateCodes.bat
@@ -1,4 +1,5 @@
 TASKKILL /IM Dolphin.exe /F
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Divaddd/dolphin-mpn/master/mpn-codes.zip', 'mpn-codes.zip')"
-7z e mpn-codes.zip -oUser\GameSettings * -y
+7za x mpn-codes.zip -oUser\GameSettings * -r -y
 del "mpn-codes.zip"
+pause
