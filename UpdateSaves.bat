f8cb6e0a5eed3a61067c70ec13ed15eddcfe1b39
Divaddd <43974972+Divaddd@users.noreply.github.com>
2021-02-14 11:09:30 -0700

    Add files via upload

--- UpdateSaves.bat
+++ UpdateSaves.bat
@@ -1,5 +1,5 @@
 TASKKILL /IM Dolphin.exe /F
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Divaddd/dolphin-mpn/master/mpn-saves.zip', 'mpn-saves.zip')"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Divaddd/dolphin-mpn/master/Replace.bat', 'Replace.bat')"
-7za x mpn-saves.zip -oUser\Backup * -r -y
+7z x mpn-saves.zip -oUser\Backup * -r -y
 del "mpn-saves.zip"
